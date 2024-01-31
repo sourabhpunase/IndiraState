3c43ca58f0132f64ec74406a61e150fe60351117
sourabh punase <sourabhpunase19@gmail.com>
2024-02-01 04:47:51 +0530

    changes and showing listings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Profile from './pages/Profile'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
+import { Listing } from './pages/Listing'
 
 export default function App() {
   return (
@@ -20,6 +21,8 @@ export default function App() {
 <Route path='/sign-in' element={<Signin/>} />
 <Route path='/sign-up' element={<Signup/>} />
 <Route path='/about' element={<About/>} />
+
+<Route path='/listing/:listingId' element={<Listing/>}/>
 <Route element={<PrivateRoute/>}>
 <Route path='/profile' element={<Profile/>} />
 <Route path='/create-listing' element={<CreateListing/>}/>
