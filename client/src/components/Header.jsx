56e18e0ab582871bbc99c39e4b9d6e342de90fa0
sourabh punase <sourabhpunase19@gmail.com>
2024-02-10 04:22:34 +0530

    update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,39 @@
 import {FaSignInAlt,FaSearch,FaBiking,FaSign,FaAddressBook,FaHome} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 
 
 export default function Header() {
   const {currentUser}=useSelector(state=>state.user)
+ const [searchTerm,setSearchTerm]=useState('');
+const navigate=useNavigate();
+
+
+ const handleChange=(e)=>{
+  setSearchTerm(e.target.value);
+
+ }
+const handleSubmit=(e)=>{
+  e.preventDefault();
+  const urlParams= new URLSearchParams(window.location.search);
+urlParams.set('searchTerm',searchTerm);
+const searchQuery=urlParams.toString();
+navigate(`/search?${searchQuery}`);
+
+
+
+
+};
+useEffect(()=>{
+  const urlParams= new URLSearchParams(window.location.search);
+  const searchTermFromUrl=urlParams.get('searchTerm');
+
+  if(searchTermFromUrl){
+    setSearchTerm(searchTermFromUrl)
+  }
+},[location.search])
+
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
@@ -13,11 +42,21 @@ export default function Header() {
 <span className='text-slate-400'>Indira </span>
 <spa className="text-rose-500">Estates</spa>
 </h1></Link>  
-<form className='bg-slate-100 p-3 rounded-lg flex items-center w-24 sm:w-64' >
+<form 
+onSubmit={handleSubmit}
+className='bg-slate-100 p-3 rounded-lg flex items-center w-24 sm:w-64' >
     <input type='text ' placeholder='Explore...'
     className='bg-transparent focus:outline-none cursor-text'
-    />
+   value={searchTerm}
+   onChange={handleChange}
+   
+   />
+   <button>
+
+
+
    <FaSearch className='text-slate-600' />
+   </button>
 </form>
 
 <ul className='flex gap-9'>
