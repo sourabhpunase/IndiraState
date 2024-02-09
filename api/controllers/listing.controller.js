bb7b95ce858c177ecbc6e41ab6eebe96ff26f5d3
sourabh punase <sourabhpunase19@gmail.com>
2024-02-10 04:04:37 +0530

    search apiroute

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -94,7 +94,7 @@ export const getListings = async (req, res, next) => {
 
     const searchTerm = req.query.searchTerm || '';
 
-    const sort = req.query.sort || 'createdAt';
+    const sort = req.query.sort||'createdAt'
 
     const order = req.query.order || 'desc';
 
