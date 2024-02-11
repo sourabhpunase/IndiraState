1c9d353ceb139c2610bd821241a7288965078fbf
sourabh punase <sourabhpunase19@gmail.com>
2024-02-11 05:35:05 +0530

    updating to render

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -62,7 +62,7 @@ fetchOffer();
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Indira Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
